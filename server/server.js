0ce870ef41dcda077ac30e317eb21d94c64446b3
kadhit <92635388+kadhit@users.noreply.github.com>
2022-06-30 14:10:58 +0700

    add

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
@@ -16,6 +17,19 @@ app.use(express.urlencoded({ extended: false }));
 // Routing
 app.use('/api/goals', require('./routes/goalRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'));
+    });
+} else {
+    app.get('/', (req, res) => {
+        res.send('Please set to production');
+    });
+}
 
 // Error Handling
 app.use(errorHandler);
